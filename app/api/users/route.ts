ec8dc85ee6b50eae3e380f0dd7ed7f0db21531af
Gabriel Muniz <gabrielmfd@gmail.com>
2024-11-07 02:38:46 -0300

    feat: add POST method to create new users via api

---
+++ app/api/users/route.ts
@@ -0,0 +1,38 @@
+import { NextResponse, NextRequest } from "next/server";
+import prisma from "@/app/_lib/prisma";
+
+export async function POST(req: NextRequest) {
+  try {
+    const { name, email, password } = await req.json();
+
+    if (!name || !email || !password) {
+      return NextResponse.json(
+        { error: "Name, email and password are required" },
+        { status: 400 },
+      );
+    }
+
+    const foundUser = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (foundUser) {
+      return NextResponse.json(
+        { error: "Email is already in use" },
+        { status: 409 },
+      );
+    }
+
+    const newUser = await prisma.user.create({
+      data: { name, email, password },
+    });
+
+    return NextResponse.json(newUser, { status: 201 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Failed to create user" },
+      { status: 500 },
+    );
+  }
+}
