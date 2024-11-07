c664ad1f54a8008e3c51dc9e1114f74edb501563
Gabriel Muniz <gabrielmfd@gmail.com>
2024-11-07 18:31:15 -0300

    chore: start LoginRegisterModal component implementation

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -8,7 +8,7 @@ const Login = () => {
   return (
     <main className="grid h-full grid-cols-2">
       <section className="relative flex flex-col items-center justify-center px-12">
-        <div className="flex w-[80%] flex-col justify-center">
+        <div className="flex max-w-xl flex-col justify-center">
           <LogoRender />
 
           <p className="my-6 text-xl text-muted-foreground">
