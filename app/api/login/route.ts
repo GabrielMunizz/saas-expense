793d46953fafbc9ab2ee124dcbbdb477e4ca6b18
Felipe Rodrigues - Neath <neath.felipe@gmail.com>
2024-11-07 18:43:27 -0300

    refactor: move files to new directory

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,5 +1,5 @@
-import { NextResponse, NextRequest } from "next/server";
-import prisma from "@/app/_lib/prisma";
+import prisma from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
   try {
