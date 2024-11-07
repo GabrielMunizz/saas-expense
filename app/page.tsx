bbd38fc6757f426871109e8a29aff5c4d4a1c2b9
Gabriel Muniz <gabrielmfd@gmail.com>
2024-11-07 01:50:22 -0300

    chore: add prisma/client

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import { Button } from './_components/ui/button';
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  redirect("/login");
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
       <h1>Home</h1>
-      <Button>Clique aqui!</Button>
     </div>
   );
 }
