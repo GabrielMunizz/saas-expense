b4b225f61cc8c4cc3f75d909bf20728504ac5384
Gabriel Muniz <gabrielmfd@gmail.com>
2024-11-09 11:39:52 -0300

    fix: fixed bug that did not redirect user to login page after logout

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,18 @@
+import { authOptions } from "@/backend/authentication/auth";
 import CashFlow from "@/components/CashFlow/CashFlow";
 import Header from "@/components/Header/Header";
 import Transactions from "@/components/Transactions/Transactions";
 import Welcome from "@/components/Welcome/Welcome";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-export default function Page() {
+const Page = async () => {
+  const session = await getServerSession(authOptions);
+
+  if (!session?.user) {
+    redirect("/login");
+  }
+
   return (
     <main className="grid h-full grid-cols-[1fr_2fr]">
       <section className="relative flex flex-col items-start justify-start border-r-2 px-12">
@@ -18,4 +27,6 @@ export default function Page() {
       </div>
     </main>
   );
-}
+};
+
+export default Page;
