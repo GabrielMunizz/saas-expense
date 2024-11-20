19a17e51e8ce87017b7fc16c21fa6f9e75438fc9
Gabriel Muniz <gabrielmfd@gmail.com>
2024-11-20 11:04:40 -0300

    feat(actions): add getTransactions server action

---
+++ backend/actions/transactions/get-transactions/index.ts
@@ -0,0 +1,23 @@
+"use server";
+
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/backend/authentication/auth";
+import prisma from "@/lib/prisma";
+
+export const getTransactions = async () => {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    throw new Error("Unauthorized");
+  }
+
+  const userId = session.user.id;
+
+  const transactions = await prisma.transaction.findMany({
+    where: {
+      userId,
+    },
+  });
+
+  return transactions;
+};
