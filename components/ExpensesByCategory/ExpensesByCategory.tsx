837b20a4ee69c9c04cde653d3d1fc95a6350293b
Gabriel Muniz <gabrielmfd@gmail.com>
2024-11-21 15:24:20 -0300

    Refactor(Dashboard): add default message in case theres no transactions

--- components/ExpensesByCategory/ExpensesByCategory.tsx
+++ components/ExpensesByCategory/ExpensesByCategory.tsx
@@ -12,21 +12,26 @@ const ExpensesByCategory = ({ transactions }: ExpensesByCategoryProps) => {
   const byExpense = calculateExpenseByCategory(transactions);
 
   return (
-    <section className="w-full rounded-lg border-2 border-muted px-8 py-6">
+    <section className="h-[433px] w-full rounded-lg border-2 border-muted px-8 py-6">
       <div>
         <h2 className="text-lg font-bold">Gastos por categoria</h2>
       </div>
       <hr className="my-4" />
-      <div className="grid grid-cols-2 gap-x-8">
-        {byExpense.map((expense) => (
-          <ByCategoryExpense
-            key={expense.category}
-            title={CATEGORY_LABELS[expense.category]}
-            amount={expense.amount}
-            percentage={expense.percentage}
-          />
-        ))}
-      </div>
+      {transactions.length !== 0 ? (
+        byExpense.map((expense) => (
+          <div key={expense.category} className="grid grid-cols-2 gap-x-8">
+            <ByCategoryExpense
+              title={CATEGORY_LABELS[expense.category]}
+              amount={expense.amount}
+              percentage={expense.percentage}
+            />
+          </div>
+        ))
+      ) : (
+        <div className="flex w-full flex-col items-center justify-center">
+          <h2 className="mt-10 text-xl font-bold">Sem gastos</h2>
+        </div>
+      )}
     </section>
   );
 };
